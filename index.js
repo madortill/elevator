2f6577e6cecee529a2ddc7815373a4d2f25e2771
madortill <madortill@gmail.com>
2024-01-24 15:09:43 +0200

    elevator

---
+++ index.js
@@ -0,0 +1,11 @@
+window.addEventListener("load", () => {
+    document.getElementById('submit', finish);
+    if (window.location.href === "end.html") {
+        
+    }
+})
+
+const finish = (event) => {
+    event.preventDefault();
+    window.location.href = "end.html"
+}
